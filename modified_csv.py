f487ff2091913d2f4ce10250112fa4f5beca2e08
Cb1ock <ch3197@qq.com>
2024-01-22 16:41:03 +0800

    train scuess

--- modified_csv.py
+++ modified_csv.py
@@ -6,7 +6,8 @@ def replace_paths_in_csv(file_path, output_file_path):
     new_path = "/data/public_datasets/MAFW/data/frames/frames"
 
     old_path_2 = "/data/ycs/AC/Dataset/MAFW/data/audio_16k"
-    new_path_2 = "/data/public_datasets/MAFW/data/audio"
+    new_path_2 = "/data/public_datasets/MAFW/data/audio/"
+    new_path_2_new = '/data/public_datasets/MAFW/audio/'
 
     # 读取CSV文件
     with open(file_path, 'r', newline='') as file:
@@ -16,11 +17,11 @@ def replace_paths_in_csv(file_path, output_file_path):
     # 替换指定行中的路径
     for row in lines:
         for i, cell in enumerate(row):
-            if old_path in cell:
+            if new_path_2 in cell:
                 #print(cell)
                 row[i] = cell.replace(old_path, new_path)
                 cell = row[i]
-                row[i] = cell.replace(old_path_2, new_path_2)
+                row[i] = cell.replace(new_path_2, new_path_2_new)
     
     # 将修改后的数据写入新的CSV文件
     with open(output_file_path, 'w', newline='') as file:
@@ -30,6 +31,7 @@ def replace_paths_in_csv(file_path, output_file_path):
 # 使用函数
 for i in range(1, 6, 1):
     for name in 'train','test':
-        file_path = f'/home/hao/Project/HiCMAE/saved/data/mafw/audio_visual/single/split0{i}/{name}.csv'  # 输入文件路径
-        output_file_path = f'/home/hao/Project/HiCMAE/saved/data/mafw/audio_visual/single/split0{i}/{name}.csv'  # 输出文件路径
+        file_path = f'saved/data/mafw/audio_visual/single/split0{i}/{name}.csv'  # 输入文件路径
+        output_file_path = f'saved/data/mafw/audio_visual/single/split0{i}/{name}.csv'  # 输出文件路径
+
         replace_paths_in_csv(file_path, output_file_path)
